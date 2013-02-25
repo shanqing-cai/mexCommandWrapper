0761bfa71cf56fe58287c23e793596fda9874ba7
shanqing-cai <scai@bu.edu>
2013-02-25 13:39:41 -0500

    Added function to focus back on the main MATLAB window shortly after the external programs begins running

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 mexCommandWrapper
 =================
 
-A MATLAB mex program for calling Windows command lines as a separate thread and without blocking MATLAB command sequence
+A MATLAB mex program for calling Windows command lines as a separate thread and without blocking MATLAB command sequence. Unlike MATLAB's built-in dos command which switches focus from the MATLAB window to the external program, this mex program brings the MATLAB main window back to foreground and focus shortly after the initiation of the external program.
+
--- mexCommandWrapper/mexCommandWrapper.cpp
+++ mexCommandWrapper/mexCommandWrapper.cpp
@@ -6,6 +6,7 @@
 /* #include <stdlib.h> */
 #include <windows.h>
 #include <process.h>
+#include <String.h>
 
 #include "stdafx.h"
 #include "mex.h"
@@ -30,12 +31,73 @@ void cmdCall_wrap(void *finfo) {
 		free((funcCallInfo *)finfo);
 }
 
+//static void ForceForegroundWindow(HWND hWnd) 
+///* This function is adapted from "SHShadow's" post at : */
+///* http://www.codeproject.com/Tips/76427/How-to-bring-window-to-top-with-SetForegroundWindo */
+//{
+//	HWND hWndFg = GetForegroundWindow();
+//    unsigned int foreThread = GetWindowThreadProcessId(hWndFg, NULL);
+//    unsigned int appThread = GetCurrentThreadId();
+//	LPWSTR fgWinTitle;
+//	BOOL attachOkay;
+//
+//	printf("ForceForegroundWindow: GetForegroundWindow() = %d\n", GetForegroundWindow());
+//	printf("ForceForegroundWindow: input window title length = %d\n", GetWindowText(hWnd, fgWinTitle, 256));
+//
+//	//DEBUG
+//	printf("foreThread = %d\n", foreThread);
+//	printf("appThread = %d\n", appThread);
+//	printf("hWnd = %d\n", hWnd);
+//
+//    if (foreThread != appThread)
+//    {
+//        //attachOkay = AttachThreadInput(foreThread, appThread, true);
+//		if (attachOkay)
+//			printf("Attaching succeeded.\n");
+//		else
+//			printf("Attaching failed.\n");
+//
+//        //BringWindowToTop(hWnd);        		
+//		//SetActiveWindow(hWnd);
+//		//SwitchToThisWindow(hWnd, true);
+//		//SwitchToThisWindow(hWnd, false);
+//		//BringWindowToTop(hWnd);
+//		//SetActiveWindow(hWnd);
+//        //ShowWindow(hWnd, SW_SHOW);
+//
+//		//ShowWindow(hWnd, SW_SHOWNORMAL);
+//		//ShowWindow(hWnd, SW_SHOWMAXIMIZED);
+//		SetForegroundWindow(hWnd);
+//        AttachThreadInput(foreThread, appThread, false);
+//    }
+//    else
+//    {
+//        BringWindowToTop(hWnd);
+//        ShowWindow(hWnd, SW_SHOW);
+//    }
+//}
+
+
+
 /* Usage of mex function: mex(cmdLine) */
 void mexFunction( int nlhs, mxArray *plhs[],
                   int nrhs, const mxArray *prhs[] ) {
-	char *cmdLine;			
+	char *cmdLine;
 	funcCallInfo *fCallInfo = NULL;
-	/* mwSize inArgDims[2]; */
+	HWND hwnd, t_hwnd;
+	/* mwSize inArgDims[2]; */	
+	LPWSTR fgWinTitle;
+	unsigned int mexThreadId = GetCurrentThreadId();
+
+	/* Find the handle to the main matlab window */
+	hwnd = GetForegroundWindow();
+	t_hwnd = GetWindow(hwnd, GW_HWNDFIRST);
+
+	//do {
+	//	//printf("t_hwnd = %d\n", t_hwnd);
+	//	GetWindowText(t_hwnd, fgWinTitle, 512);
+	//} while (t_hwnd = GetWindow(t_hwnd, GW_HWNDNEXT));
+	/* ~Find the handle to the main matlab window */
 
 	if (nrhs < 1) {
 		mexErrMsgTxt("Incorrect input argument format");
@@ -57,5 +119,9 @@ void mexFunction( int nlhs, mxArray *plhs[],
 	/* cmdCall_wrap(fCallInfo); */
 	_beginthread(cmdCall_wrap, 0, (void *)fCallInfo);
 
-	
+	//printf("hwnd = %d\n", hwnd);
+	//printf("mexThreadId = %d\n", mexThreadId);
+	Sleep(80);
+	//ForceForegroundWindow(hwnd);
+	SetForegroundWindow(hwnd);
 }
